173a522a3db7dbb82c60a61fd865191fd942dc9a
kencoppola <ken@iplaycornhole.com>
2021-02-26 23:05:20 -0500

    All of the number are now looping in the calendar on the correct rows.

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -10,22 +10,35 @@ export class CalendarComponent implements OnInit {
   date = new Date();
   month = this.date.getMonth();
   fullMonth: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July ', 'August', 'September', 'October', 'November', 'December'];
+  fullDaysOfTheWeek: string[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   displayedMonth: string = '';
   year = this.date.getFullYear();
   firstDay = (new Date(this.year, this.month)).getDay();
+  firstDayOfThePreviousMonth: Date;
   firstOfTheCurrentMonth: Date;
+  daysInTheCurrentMonth: number = 28;
   firstOfNextMonth: Date;
+  numberOfDays: number[] = [];
  
   constructor() { }
 
   ngOnInit(): void {
-    this.getTheFirstOfNextMonth();
-    this.getTheFirstOfTheCurrentMonth()
     this.displayMonth();
+    this.getTheFirstOfThePreviousMonth();
+    this.getTheFirstOfTheCurrentMonth();
+    this.startTheCurrentMonthOnTheCorrectDay();
+    this.getTheFirstOfNextMonth();
+    
   }
 
   displayMonth () {
    return this.displayedMonth =  this.fullMonth[this.month];
+  }
+
+  getTheFirstOfThePreviousMonth() {
+    let firstDayOfThePreviousMonth = new Date();
+    firstDayOfThePreviousMonth.setMonth(firstDayOfThePreviousMonth.getMonth()-1, 1);
+    return this.firstDayOfThePreviousMonth = firstDayOfThePreviousMonth;
   }
 
   getTheFirstOfTheCurrentMonth() {
@@ -38,5 +51,21 @@ export class CalendarComponent implements OnInit {
     let firstOfNextMonth = new Date();
     firstOfNextMonth.setMonth(firstOfNextMonth.getMonth()+1, 1);
     return this.firstOfNextMonth = firstOfNextMonth;
+  }
+
+  startTheCurrentMonthOnTheCorrectDay() {
+    let splitCurrentMonth = this.firstOfTheCurrentMonth.toString().split(' ');
+    if (splitCurrentMonth[0] === 'Mon') {
+      console.log('hello');
+    }
+
+    if(this.daysInTheCurrentMonth) {
+      for (let i = 1; i<= this.daysInTheCurrentMonth; i++){ 
+        this.numberOfDays.push(i);
+      }
+      console.log(this.numberOfDays);
+    }
+
+    console.log(splitCurrentMonth);
   }
 }
